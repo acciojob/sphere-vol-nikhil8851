dd5209b69ec375bf3632e48836d7230c83327596
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-10-12 23:07:34 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
+event.preventDefault()
+	
+  let radius = document.querySelector(`#radius`).value
+
+	radius = parseFloat(radius)
+if(isNaN(radius) || radius < 0){
+	 document.querySelector(`#volume`).value = `NaN`
+}else{
+	let volume = (4/3)*Math.PI*Math.pow(radius,3)
+	volume = volume.toFixed(4)
+	document.querySelector(`#volume`).value = volume
+}
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function(){ document.getElementById('MyForm').onsubmit = volume_sphere}
